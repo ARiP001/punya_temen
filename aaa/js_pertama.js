a7576de4ab14289c08d1b35619aa770502b5f07b
ARiP001 <142637487+ARiP001@users.noreply.github.com>
2023-11-16 05:09:11 +0700

    Delete aaa directory

--- aaa/js_pertama.js
+++
@@ -1,6 +0,0 @@
-const navbaricon = document.querySelector ("#navbar-icon");
-
-navbaricon.addEventListener("click",function(){
-const navbargroup = document.querySelector("#navbar-group");
-navbargroup.classList.toggle("btn-groups");
-});
